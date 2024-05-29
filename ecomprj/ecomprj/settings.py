9e77bcfcac76b595769a8085e87141e39f39b0df
aslamnahas <nahasnazzz06@gmail.com>
2024-05-29 21:55:39 +0530

    settings dir

--- ecomprj/ecomprj/settings.py
+++ ecomprj/ecomprj/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
